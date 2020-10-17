ff903fd82c35c3864515c69fa2c4bc1418d37d5c
Edwin Quispe <edwinqramos@gmail.com>
2020-10-16 20:51:10 -0500

    Aplicacion Front React

---
+++ precache-manifest.b616883fda10d9208be039731b46fabf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5df3d63d28ce538803c93d41b0dd468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d19378d72809d936fb7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e928ea22e510e51787c2",
+    "url": "/static/js/2.e11377ba.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e11377ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d19378d72809d936fb7",
+    "url": "/static/js/main.62b0d275.chunk.js"
+  },
+  {
+    "revision": "6bd4c7f923135e7ccdf0",
+    "url": "/static/js/runtime-main.8992646a.js"
+  }
+]);
